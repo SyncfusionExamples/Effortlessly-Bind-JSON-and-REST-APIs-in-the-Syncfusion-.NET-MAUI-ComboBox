d049a4974f86c6764d848170c97cd054105cb8e0
sudarsanMS <sudarsan.m@syncfusion.com>
2025-11-27 09:55:12 +0530

    changes

--- ComboBoxSample/ComboBoxSample/UsingRESTAPIs.xaml.cs
+++ ComboBoxSample/ComboBoxSample/UsingRESTAPIs.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
@@ -23,9 +23,19 @@ namespace ComboBoxSample
 			if (BindingContext is RestViewModel vm)
 				await vm.LoadAsync();
 		}
+
+        private async void OnCreateClicked(object sender, EventArgs e)
+        {
+			if (BindingContext is RestViewModel vm)
+			{
+				var name = NameEntry?.Text?.Trim();
+				if (!string.IsNullOrWhiteSpace(name))
+					await vm.CreateAsync(name);
+			}
+		}
 	}
 
-	public class UserDto
+    public class UserDto
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
@@ -35,16 +45,31 @@ namespace ComboBoxSample
 	{
 		public ObservableCollection<UserDto> Users { get; } = new();
 
+		private readonly HttpClient http = new HttpClient { Timeout = TimeSpan.FromSeconds(20) };
+		private const string BaseUrl = "https://jsonplaceholder.typicode.com/users";
+
 		public async Task LoadAsync()
 		{
-			using var http = new HttpClient { Timeout = TimeSpan.FromSeconds(20) };
-			var url = "https://jsonplaceholder.typicode.com/users";
-
-			var data = await http.GetFromJsonAsync<List<UserDto>>(url);
+			var data = await http.GetFromJsonAsync<List<UserDto>>(BaseUrl);
 
 			Users.Clear();
 			foreach (var u in data.OrderBy(u => u.Name))
 				Users.Add(u);
+		}
+
+		public async Task CreateAsync(string name)
+		{
+			var newUser = new UserDto { Name = name };
+			var resp = await http.PostAsJsonAsync(BaseUrl, newUser);
+			resp.EnsureSuccessStatusCode();
+
+			// JSONPlaceholder returns the created resource with an id
+			var created = await resp.Content.ReadFromJsonAsync<UserDto>();
+			if (created != null)
+			{
+				// Add to the top
+				Users.Insert(0, created);
+			}
 		}
 	}
 }
